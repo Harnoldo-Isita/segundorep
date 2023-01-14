c9c00fa3a55ef76a6b1cc302be4c4de116f6eec1
Harnoldo-Isita <121909247+Harnoldo-Isita@users.noreply.github.com>
2023-01-14 09:19:11 -0700

    Cambios a inicio clase persona

--- inicio.php
+++ inicio.php
@@ -1,3 +1,14 @@
 <?php
   echo "Ejemplo de pull"
+    
+   $persona = [
+                "Nombre" => "Pedro",
+                "apellido" => "Sanchez",
+                "edad" => 50
+              ];
+
+  foreach($persona as $valor){
+    echo "$valor <br>";
+  }
+    
 ?>
